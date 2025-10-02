519d58fa3db02bca67ef67d917bdad55dfdbefca
Leo Blum <143550009+LeoB1um@users.noreply.github.com>
2025-10-01 23:02:36 -0400

    Parsing Apache Logs

---
+++ week4-5/parsing apache logs.ps1
@@ -0,0 +1,28 @@
+﻿function ApacheLogs1() {
+$logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+
+$tableRecords = @()
+
+for($i = 0; $i -lt $logsNotFormatted.Count; $i++){
+
+#split string into words
+$words = $logsNotFormatted[$i].Split(" ");
+
+#Write-Host "words: $words"
+ $tableRecords += [PSCustomObject]@{ "IP" = $words[0];
+                                     "Time" = $words[3].Trim('[');
+                                     "Method" = $words[5].Trim('"');
+                                     "Page" = $words[6];
+                                     "Protocol" = $words[7];
+                                     "Response" = $words[8];
+                                     "Referrer" = $words[10];
+                                     "Client" = $words[11..($words.Length -1)]; 
+                                     }
+} # end of for loop
+return $tableRecords | Where-Object { $_.IP -ilike "10.*"}
+}
+
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
+
